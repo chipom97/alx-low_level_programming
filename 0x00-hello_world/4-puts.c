4a2c885abcf1ffb372dd8b326d3ccf3bde52a5e2
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-07 12:47:53 -0700

    Task 4 completed

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+* main - entry point of the program
+* Return: - zero
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle);
 return (0);
 }
