da023902bcbc554136ad39b718e95828375ec214
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-08 11:58:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+	putchar(i + '0');
+	}
+	putchar('\n');
+	return (0);
+}
