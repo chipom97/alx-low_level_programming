bd427bfd78020c30a2c51899637243d0027a539b
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-10 14:19:07 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*** main - entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+perror ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
