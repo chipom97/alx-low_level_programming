1c02e2df298b300fcae876036567b1ff16737106
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-14 16:15:29 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* main - check the code
+*
+* Return: 0 not uppercase
+*         1 uppercase
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
