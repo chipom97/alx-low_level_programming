6a2279496d0e1eb131a3b2050b8597dafabe1d5c
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-21 08:29:39 -0700

    101-natural.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: always (0) success
+*/
+int main(void)
+{
+int i;
+int sum = 0;
+
+for (i = 1; i < 1024; i++)
+{
+if (i % 3 == 0 || i % 5 == 0)
+{
+sum += i;
+}
+}
+printf("%d\n", sum);
+return (0);
+}
