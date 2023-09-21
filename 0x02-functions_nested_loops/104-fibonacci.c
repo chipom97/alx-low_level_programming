3c0a9ff88050972bfe577d17d3a24dbd0769f562
Chipo Mazhani <chipomazhani@gmail.com>
2023-09-21 10:54:40 -0700

    104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry point
+*
+* Return: always (0) success
+*/
+int main(void)
+{
+unsigned long a = 1, b = 2, c;
+int count = 2;
+
+printf("%lu, %lu", a, b);
+while (count < 98)
+{
+c = a + b;
+if (count < 98)
+{
+printf(",");
+}
+printf(" %lu", c);
+a = b;
+b = c;
+count++;
+}
+printf("\n");
+return (0);
+}
