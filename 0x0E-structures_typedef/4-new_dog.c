d14af519c5a6da641f6affc40591502bf9100f43
Chipo Mazhani <chipomazhani@gmail.com>
2023-10-10 13:57:53 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -7,7 +7,7 @@
 * @name: name
 * @age: age
 * @owner: owner
-* Return: always 0
+* Return: new dog
 */
 dog_t *new_dog(char *name, float age, char *owner)
 {
@@ -20,8 +20,8 @@ new_dog->name = strdup(name);
 new_dog->owner = strdup(owner);
 if (new_dog->name == NULL || new_dog->owner == NULL)
 {
-free(new_dog->name);
 free(new_dog->owner);
+free(new_dog->name);
 free(new_dog);
 return (NULL);
 }
