497ca9fe44149268ceed54a174f7ff7fbce629d3
Chipo Mazhani <chipomazhani@gmail.com>
2023-10-25 08:04:34 -0700

    101-wildcmp.c

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -5,24 +5,26 @@
  * @s1: The first string to compare.
  * @s2: The second string with wildcards.
  *
- * Return: 1 if the strings can be considered identical, otherwise return 0.
+ * Return: 1, otherwise return 0.
  */
 int wildcmp(char *s1, char *s2)
 {
-if (*s2 == '\0')
-return (*s1 == '\0');
 if (*s2 == '*')
 {
-if (*(s2 + 1) == '*')
-return (wildcmp(s1, s2 + 1));
+while (*s2 == '*')
+s2++;
+if (*s2 == '\0')
+return (1);
 while (*s1 != '\0')
 {
-if (wildcmp(s1, s2 + 1))
+if (wildcmp(s1, s2))
 return (1);
-s1++;  /* Move to the next character in s1. */
+s1++;
 }
-return (wildcmp(s1, s2 + 1));
+return (0);
 }
+if (*s1 == '\0' || *s2 == '\0')
+return ((*s1 == *s2));
 if (*s1 == *s2 || *s2 == '?')
 return (wildcmp(s1 + 1, s2 + 1));
 return (0);
