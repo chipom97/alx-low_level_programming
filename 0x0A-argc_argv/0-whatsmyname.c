329bda35874432c3de0aa354ae1438f7e2266259
Chipo Mazhani <chipomazhani@gmail.com>
2023-10-27 03:38:06 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc > 0)
+{
+printf("%s\n", argv[0]);
+}
+return (0);
+}
