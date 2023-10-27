d6c0ee966aade2e0d922e2f5f8674070ceb156a1
Chipo Mazhani <chipomazhani@gmail.com>
2023-10-27 04:22:54 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array containing the command-line arguments
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
