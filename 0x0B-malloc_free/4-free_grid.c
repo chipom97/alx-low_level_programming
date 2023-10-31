d9e10f77355fd07878e319f4f4639598a5d481f9
Chipo Mazhani <chipomazhani@gmail.com>
2023-10-31 10:12:56 -0700

    4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Free a grid
+ * @grid: The grid to be freed
+ * @height: The height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+int i;
+for (i = 0; i < height; i++)
+{
+free(grid[i]);
+}
+free(grid);
+}
