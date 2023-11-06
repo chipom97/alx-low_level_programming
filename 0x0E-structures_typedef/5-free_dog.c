a33d7652213abba15e4d4cb283e5d0032536cc6f
Chipo Mazhani <chipomazhani@gmail.com>
2023-11-06 06:29:55 -0800

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees the memory
+ * @d: A pointer to the dog structure to be freed
+ */
+void free_dog(dog_t *d)
+{
+if (d == NULL)
+{
+return;
+}
+if (d->name != NULL)
+{
+free(d->name);
+}
+if (d->owner != NULL)
+{
+free(d->owner);
+}
+free(d);
+}
