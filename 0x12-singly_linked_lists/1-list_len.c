a59378250e398683897e5567f656f9a24195e481
Chipo Mazhani <chipomazhani@gmail.com>
2023-11-17 12:33:12 -0800

    1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+printf("[%d] %s\n", h->len, (h->str != NULL) ? h->str : "(nil)");
+h = h->next;
+count++;
+}
+return (count);
+}
+
+/**
+ * list_len - returns the number of elements in a list_t list.
+ * @h: pointer to the head of the list
+ *
+ * Return: the number of elements
+ */
+size_t list_len(const list_t *h)
+{
+size_t count = 0;
+while (h != NULL)
+{
+count++;
+h = h->next;
+}
+return (count);
+}
