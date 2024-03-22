c729165c0723f4bc0537ac979ba7766915ce9c13
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-22 07:41:06 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: (0) Success.
+ */
+int main(void)
+{
+char letter;
+int i;
+
+for (i = 0; i <= 9; i++)
+{
+putchar(i + '0');
+}
+for (letter = 'a'; letter <= 'f'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
+}
