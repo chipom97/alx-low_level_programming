d25981b78b47895874dd9bec92f76daa92840f94
Chipo Mazhani <chipomazhani@gmail.com>
2024-03-23 06:59:08 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: character
+ * Return: (0) Success.
+ */
+int _islower(int c)
+{
+int i;
+
+for (i = 97; i <= 122; i++)
+{
+if (c == i)
+{
+return (1);
+}
+}
+return (0);
+}
