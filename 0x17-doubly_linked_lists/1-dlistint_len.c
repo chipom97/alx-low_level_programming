a77a6d4db4984e3f2f69d58f96d289276d4da532
Chipo Mazhani <chipomazhani@gmail.com>
2024-04-01 07:08:17 -0700

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * dlistint_len - return number of elements
+ * @h: head pointer
+ * Return: Always EXIT_SUCCESS.
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+size_t n;
+
+n = 0;
+while (h != NULL)
+{
+h = h-> next;
+n++;
+}
+return (n);
+}
