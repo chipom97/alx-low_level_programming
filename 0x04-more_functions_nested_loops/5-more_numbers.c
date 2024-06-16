2d131de0ccbfde0048cfbb31d4840e0678db4f48
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-16 16:37:19 +0000

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,11 @@ for (j = 0; j <= 9; j++)
 {
 for (i = 0; i <= 14; i++)
 {
-if(i > 9)
+if (i > 9)
 {
 putchar('0' + (i / 10));
 }
-putchar('0' + (i %10));
+putchar('0' + (i % 10));
 }
 putchar('\n');
 }
