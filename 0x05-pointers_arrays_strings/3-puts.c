f6e4061858bdaa813a68a663ecdda59c659619ad
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-18 17:59:56 +0000

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print a string
+ * @str: sting
+ * Return: string
+ */
+
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*str);
+str++;
+}
+_putchar('\n');
+}
