ea36321fa87d6628cb0de2bad571c53c2bfca4bd
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-19 10:55:37 +0000

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,4 +10,20 @@
 
 char *_strcat(char *dest, char *src)
 {
+int index = 0;
+int copy = 0;
+
+while (dest[index] != '\0')
+{
+index++;
+}
+
+while (src[copy] != '\0')
+{
+dest[index] = src[copy];
+index++;
+copy++;
+}
+dest[index] = '\0';
+return (dest);
 }
