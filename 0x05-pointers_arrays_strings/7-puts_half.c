6ccb26659571c848d465e2255492dd0a25ddb53c
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-19 08:50:28 +0000

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * puts_half - print half string
+ * @str: string
+ * Return: half string
+ */
+
+void puts_half(char *str)
+{
+int len = 0;
+int index;
+
+while (str[len] != '\0')
+{
+len++;
+}
+
+if (len % 2 == 0)
+{
+index = (len / 2);
+}
+
+else
+{
+index = ((len - 1) / 2);
+}
+
+while (str[index] != '\0')
+{
+putchar(str[index]);
+index++;
+}
+putchar('\n');
+}
