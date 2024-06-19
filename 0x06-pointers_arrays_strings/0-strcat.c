a422a5f19f4a9f9bf73f01bafdf3a5d2f8a7e26f
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-19 10:29:01 +0000

    created filrsd

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *char *_strcat - concatenate two strings
+ * @dest: resulting string
+ * @src: string
+ * Return: string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+}
