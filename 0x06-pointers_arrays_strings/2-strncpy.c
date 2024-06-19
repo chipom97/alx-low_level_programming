0159b8acadb633d1b1444463ce3ec26b201b6f72
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-19 12:46:40 +0000

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strncpy - copy string
+ * @dest: destination string
+ * @src: source string
+ * @n: integer
+ * Return: string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int copy = 0;
+
+while (src[copy] != '\0' && copy < n)
+{
+dest[copy] = src[copy];
+copy++;
+}
+
+while (copy < n)
+{
+dest[copy] = '\0';
+copy++;
+}
+
+return (dest);
+}
