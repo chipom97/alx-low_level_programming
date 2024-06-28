bf85bb21f651190bbb2becaf4ceee72fdf1b029e
Chipo Mazhani <chipomazhani@gmail.com>
2024-06-28 07:27:36 +0000

    task_1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - check for digit
+ * @c: digit
+ * Return:  1 for digit, else 0.
+ */
+
+int _isdigit(int c)
+{
+if (c >= 48 && c <= 57)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
