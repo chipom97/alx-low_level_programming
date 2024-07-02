6b59952eb0eaf8e299eb20551d049ebf522df86c
Chipo Mazhani <chipomazhani@gmail.com>
2024-07-02 07:36:17 +0000

    tasl

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,6 +6,25 @@
  * Return: string.
  */
 
+
 char *leet(char *str)
 {
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == 'a' || str[i] == 'A')
+			str[i] = '4';
+		if (str[i] == 'e' || str[i] == 'E')
+			str[i] = '3';
+		if (str[i] == 'o' || str[i] == 'O')
+			str[i] = '0';
+		if (str[i] == 't' || str[i] == 'T')
+			str[i] = '7';
+		if (str[i] == 'l' || str[i] == 'L')
+			str[i] = '1';
+		i++;
+	}
+	return (str);
 }
+
