c8a684b49e22254a59bd03fb87dd3b8eeb88ec69
Chipo Mazhani <chipomazhani@gmail.com>
2025-03-29 21:06:59 +0200

    task_2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet ten times
+ *
+*/
+
+void print_alphabet_x10(void)
+{
+	int i;
+	int counter = 0;
+
+	while (counter != 10)
+	{
+		for (i = 97; i <= 122; i++ )
+		{
+			_putchar(i);
+		}
+		counter++;
+		_putchar('\n');
+	}
+}
