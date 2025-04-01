fbe55e27e1ca4968f85daeee1595e7991d753311
Chipo Mazhani <chipomazhani@gmail.com>
2025-04-01 22:41:42 +0200

    task_7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: integer
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 2; j <= i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
