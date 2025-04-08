f5a2bd9029e3d8f86f3281eec738467113d7758d
Chipo Mazhani <chipomazhani@gmail.com>
2025-04-08 08:39:43 +0200

    task_7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -5,7 +5,24 @@
  * @s: string
  * Return: string
  */
-char *leet(char *)s;
+char *leet(char *s)
 {
+	char *og_s = s;
+	int i = 0, j;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "4433007711";
 
+	while (s[i] != '\0')
+	{
+		for (j = 0; let[j] != '\0'; j++)
+		{
+			if (s[i] == let[j])
+			{
+				s[i] = num[j];
+				break;
+			}
+		}
+		i++;
+	}
+	return (og_s);
 }
