fc1e2241d3551c1ded99e2edf36a113d82979fbc
Chipo Mazhani <chipomazhani@gmail.com>
2025-04-10 13:36:30 +0200

    100-times-table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,61 +3,55 @@
 /**
  * print_times_table - print times table
  * @n: integer
+ * reduce the number of lines by calculating the variables
+ * inside the _putchar line
  * Return: Always 0.
  */
 
 void print_times_table(int n)
 {
-int row, col, p, p1, p2, p0;
+    int row, col, p;
 
-if  (!(n < 0 || n > 15))
-{
-for (row = 0; row <= n; row++)
-{
-for (col = 0; col <= n; col++)
-{
-p = (row * col);
-p2 = (p % 10);
-if (p >= 100)
-{
-p0 = (p / 100);
-p1 = ((p / 10) % 10);
-{
-_putchar(',');
-_putchar(' ');
-_putchar('0' + p0);
-_putchar('0' + p1);
-_putchar('0' + p2);      
-} 
-}
-else if (p >= 10)
-{
-p1 = (p / 10);
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + p1);
-_putchar('0' + p2);
-} 
-}
-else
-{
-if (col == 0)
-{
-_putchar('0' + p);
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(' ');
-_putchar('0' + p);
-}
-}
-}
-_putchar('\n');
-}
-}
+    if  (!(n < 0 || n > 15))
+    {
+        for (row = 0; row <= n; row++)
+        {
+            for (col = 0; col <= n; col++)
+            {
+                p = (row * col);
+                if (p >= 100)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar('0' + (p / 100));
+                    _putchar('0' + ((p / 10) % 10));
+                    _putchar('0' + (p % 10));
+                }
+                else if (p >= 10)
+                {
+                    _putchar(',');
+                    _putchar(' ');
+                    _putchar(' ');
+                    _putchar('0' + (p / 10));
+                    _putchar('0' + (p % 10));
+                }
+                else
+                {
+                    if (col == 0)
+                    {
+                        _putchar('0' + p);
+                    }
+                    else
+                    {
+                        _putchar(',');
+                        _putchar(' ');
+                        _putchar(' ');
+                        _putchar(' ');
+                        _putchar('0' + p);
+                    }
+                }
+            }
+            _putchar('\n');
+        }
+    }
 }
