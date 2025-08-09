5762acc4b3d952faaa81415f78d4faf0f4cfa334
Chipo Mazhani <chipomazhani@gmail.com>
2025-08-09 12:32:25 +0200

    task_0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ * @b: bytes
+ *
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(b);
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
