fa2872a60aac9ba6cfec034c0a3ac8a018611252
Chipo Mazhani <chipomazhani@gmail.com>
2025-08-10 22:27:16 +0200

    task_3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -18,4 +18,7 @@ char *owner;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/* Alias struct dog to dog_t */
+typedef struct dog dog_t;
+
 #endif
