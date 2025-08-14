bcc533ff5718cca522f74fcbb6eca3832b50b318
Chipo Mazhani <chipomazhani@gmail.com>
2025-08-14 19:08:01 +0200

    task_)

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: name of the person
+ * @f: pointer function
+ *
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL  || f != NULL)
+	{
+		f(name);
+	}
+}
