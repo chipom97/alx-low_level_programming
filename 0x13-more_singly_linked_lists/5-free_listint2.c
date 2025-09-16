328ef85fd9233849b0a3ab70ee596954660f4811
Chipo Mazhani <chipomazhani@gmail.com>
2025-09-16 13:17:23 +0200

    try again

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,16 +8,17 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *p = *head;
+	listint_t *p;
 
 	if (head == NULL)
 		return;
 
-	while (*head != NULL)
+	while (*head)
 	{
 		p = (*head)->next;
 		free(*head);
 		*head = p;
 	}
+
 	head = NULL;
 }
