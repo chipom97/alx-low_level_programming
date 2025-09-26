86abe7fc0c5971cfb228622f55a76abe7f707b0a
Chipo Mazhani <chipomazhani@gmail.com>
2025-09-26 11:36:14 +0200

    task_1_corrected

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,19 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
-		print_binary(n >> 1);
+	static int started;
+	int bit;
 
-	printf("%ld", n & 1);
+	if (n == 0 && !started)
+	{
+		_putchar('0');
+		return;
+	}
+	else if  (n == 0)
+		return;
+
+	started = 1;
+	bit = (n & 1);
+	print_binary(n >> 1);
+	_putchar('0' + bit);
 }
